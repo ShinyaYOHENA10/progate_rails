9fbbec890e17611a714f85ebd1529afba5ef9e87
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-09-10 17:41:29 +0900

    投稿詳細ページ作成した

--- twitter_app/config/routes.rb
+++ twitter_app/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
   get '/' =>"home#top"
   get "about" => "home#about"
-  get "posts/index" => "posts/index"
+  get "posts/index" => "posts#index"
+  get "posts/:id" => "posts#show"
 end
