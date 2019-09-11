e0fa849b50515d057ded9c49c484cdd3e5e44f51
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-09-12 00:23:43 +0900

    修行コース2-7maで

--- twitter_app/config/routes.rb
+++ twitter_app/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "posts/:id" => "posts#show"
   get "posts/:id/edit" => "posts#edit"
   post "posts/create" => "posts#create"
+  post "posts/:id/update" => "posts#update"
   get "/" => "home#top"
   get "about" => "home#about"
 end
