4f8f90e9a00f875b63518b4ed83ff2a3d0f9f933
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-09-17 14:41:39 +0900

    学習6-8 垢新規登録 postメソッドでサーバーへ

--- twitter_app/app/controllers/users_controller.rb
+++ twitter_app/app/controllers/users_controller.rb
@@ -2,13 +2,20 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
-  # showアクションを追加してください
+  
   def show
     @user = User.find_by(id: params[:id])
   end
-
-  # newアクションを追加してください
+  
   def new
+  end
+  
+  # createアクションを作成してください
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email])
+    @user.save
+    redirect_to("/users/#{@user.id}")
   end
 end
--- twitter_app/config/routes.rb
+++ twitter_app/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  post "users/create" => "users#create"
   get 'users/index'
   get "users/:id" => "users#show" 
-  # アカウント作成　URLはsignup,アクションはnew
   get "signup" => "users#new"
+ 
+
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
