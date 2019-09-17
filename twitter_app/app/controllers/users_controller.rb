f9631b09918ccfd69a0829c4381ff57658265416
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-09-17 15:13:28 +0900

    学習6-9 垢作成時のif文とflashメッセージ

--- twitter_app/app/controllers/users_controller.rb
+++ twitter_app/app/controllers/users_controller.rb
@@ -8,14 +8,21 @@ class UsersController < ApplicationController
   end
   
   def new
+    # Userインスタンスを作成し、変数@userに代入してください
+    @user = User.new
+    
   end
   
-  # createアクションを作成してください
   def create
-    @user = User.new(
-      name: params[:name],
-      email: params[:email])
-    @user.save
-    redirect_to("/users/#{@user.id}")
+    @user = User.new(name: params[:name], email: params[:email])
+    # 保存が成功したかどうかで条件分岐をしてください
+    if @user.save
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      # renderのパスはコントローラとアクション 文頭スラ無し
+      render("users/new")
+    end
   end
+  
 end
--- twitter_app/config/routes.rb
+++ twitter_app/config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   post "users/create" => "users#create"
-  get 'users/index'
-  get "users/:id" => "users#show" 
   get "signup" => "users#new"
- 
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
 
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
-  get "posts/:id/edit" => "posts#edit"
   post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
+  
   get "/" => "home#top"
   get "about" => "home#about"
 end
