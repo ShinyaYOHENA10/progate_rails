8804b335b4680e13bf9b2d09fd808c4c41cc190c
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-09-19 20:41:00 +0900

    道場3-1~3 カラムの追加,ユーザーアイコン表示 難しいので一旦スキップ

--- twitter_app/app/controllers/users_controller.rb
+++ twitter_app/app/controllers/users_controller.rb
@@ -10,9 +10,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
+  # 新規登録時にデフォルトアイコンを持たせる
+  #画像ファイルにクウォート付けるのを忘れない！
   def create
-    @user = User.new(name: params[:name], email: params[:email])
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      image_name: "default_user.jpg"
+    )
     if @user.save
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
