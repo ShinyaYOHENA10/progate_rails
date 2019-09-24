79f3e0e6dbc629a1bee8161ed78b2725e7756522
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-09-24 17:36:32 +0900

    学習8-3 パスワードカラム追加

--- twitter_app/app/models/user.rb
+++ twitter_app/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     validates :name, {presence: true}
     validates :email, {presence: true, uniqueness: true}
+    # パスワードカラムにバリデーション追加(空欄NG)
+    validates :password, {presence: true}
 end
--- twitter_app/db/migrate/20190919111413_add_image_name_to_users.rb
+++ twitter_app/db/migrate/20190919111413_add_image_name_to_users.rb
@@ -1,4 +1,8 @@
 class AddImageNameToUsers < ActiveRecord::Migration[6.0]
+  # rails cで "rails g migration ファイル名"
+  # change内でカラム追加
+  # "rails db:migrate"でDBへ反映
+  # パスワード設定を試す際はUser.newでオブジェクト作成 or find_byメソッドで既存のアカウントにパスワード代入
   def change
     add_column :users, :image_name, :string
   end
