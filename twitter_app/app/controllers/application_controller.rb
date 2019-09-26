b1f7ab0dc34bf78e311a3aa9c83abf43db4ddbb8
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-09-26 16:23:47 +0900

    8-11 共通する定義をapp_controllerへ

--- twitter_app/app/controllers/application_controller.rb
+++ twitter_app/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
-end
+    # before_actionにset_current_userメソッドを指定し、各コントローラで実行されるようにする
+    before_action :set_current_user
+    
+    # 共通アクションはapplication_controller.rbで定義
+    def set_current_user
+      @current_user = User.find_by(id: session[:user_id])
+    end
+  
+  end
+  
