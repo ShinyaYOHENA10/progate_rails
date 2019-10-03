0f53e3007f43eff5705da7c671dd98bd19649673
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-10-03 18:34:50 +0900

    9-5 post.rbにﾕｰｻﾞ情報を返すｲﾝｽﾀﾝｽﾒｿｯﾄﾞ追加

--- twitter_app/app/models/post.rb
+++ twitter_app/app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ApplicationRecord
     validates :content, {presence: true, length: {maximum: 140}}
-    # user_idカラムにバリデーションを追加
     validates :user_id, {presence: true}
+    
+    # Postモデル内にその投稿に紐付いたuserインスタンスを戻り値として返すuserメソッドを定義
+    # インスタンスメソッド内ではselfはインスタンス自身を表す 
+    def user
+      return User.find_by(id: self.user_id)
+    end
     
   end
   
