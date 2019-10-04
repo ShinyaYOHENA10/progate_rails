ea42c63c7789f24dbd5c76609d5eb5aa90177962
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-10-04 19:06:38 +0900

    9-8,9 whereﾒｿｯﾄﾞで全投稿を取得

--- twitter_app/app/models/user.rb
+++ twitter_app/app/models/user.rb
@@ -4,3 +4,9 @@ class User < ApplicationRecord
     # パスワードカラムにバリデーション追加(空欄NG)
     validates :password, {presence: true}
 end
+
+# ﾕｰｻﾞに紐づく全投稿取得のためwhereﾒｿｯﾄﾞを使用
+# find_byでは1件ずつ取得となる　selfはインスタンス自体を指す
+def posts
+    return User.where(user_id:self.id)
+end
