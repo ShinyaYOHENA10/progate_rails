38f3cc494a0f1c93d2d08683599dc2dd68fc2fc5
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-10-07 15:27:43 +0900

    11-1,2,3 gem bcryptでﾊﾟｽﾜｰﾄﾞのﾊｯｼｭ化

--- twitter_app/app/models/user.rb
+++ twitter_app/app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+    def has_secure_password
+    end
+
     validates :name, {presence: true}
     validates :email, {presence: true, uniqueness: true}
     # パスワードカラムにバリデーション追加(空欄NG)
-    validates :password, {presence: true}
+    # Since has_secure_password method above, line 2, has the same validation, delete it below.
+    # validates :password, {presence: true}
 end
 
 # ﾕｰｻﾞに紐づく全投稿取得のためwhereﾒｿｯﾄﾞを使用
